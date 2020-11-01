924194988aa06aa17ffb554a3faa4d1e4a9504e2
Enzo D'Onofrio <enzo@EnzoAppasterMBP.fritz.box>
2020-11-01 15:07:05 +0100

    Reset Project

--- src/constants/connections.js
+++
@@ -1,7 +0,0 @@
-const connections = {
-  HOST: 'http://localhost',
-  PORT: '3000',
-  IMAGES_STORAGE: '/',
-}
-
-export default connections
--- src/redux/reducers/projects.js
+++
@@ -1,9 +0,0 @@
-import { createReducer } from '../lib/ReducerHelper'
-
-const initialState = { d4: '0.0.1' }
-
-const projects = createReducer(initialState, {
-  GET_PROJECTS: ({ projects }) => projects,
-})
-
-export default projects
